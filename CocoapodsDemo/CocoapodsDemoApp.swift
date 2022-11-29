715de0737ac769f3312505151c4df3ae7c61e7bd
Matt Wylder <mwylder@paypal.com>
2022-11-29 09:57:01 -0600

    Initial Commit

---
+++ CocoapodsDemo/CocoapodsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocoapodsDemoApp.swift
+//  CocoapodsDemo
+//
+//  Created by Matthew Wylder on 11/29/22.
+//
+
+import SwiftUI
+
+@main
+struct CocoapodsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
